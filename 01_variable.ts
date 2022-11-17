bc1d222bfa627020d1a941b75387a34992ae2d71
Dpawar2310 <dipalisanpawar@gmail.com>
2022-11-17 21:36:48 +0530

    Create 01_variable.ts

---
+++ 01_variable.ts
@@ -0,0 +1,26 @@
+var n1: number = 10;
+console.log(n1);
+
+var s1: string = "The Typescript Program";
+var s2: string = "The Powerfull Peoples Come for Powerfull Places";
+console.log(s1);
+console.log(s2);
+ 
+var a1: any ={
+    productId : 1,
+    productName : "iphone",
+    productPrice : 25000
+}
+console.log(a1);
+
+//homogenous array
+var arr1 : Array<string> =['angular','javascript','react'];
+console.log(arr1);
+
+//hetrogenours array
+var arr2 : Array<any> =['angular',125455,'react'];
+console.log(arr2);
+
+
+
+
