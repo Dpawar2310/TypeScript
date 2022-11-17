3d85805dae7d0e931cad68aa4897b5bf66aedad0
Dpawar2310 <dipalisanpawar@gmail.com>
2022-11-17 21:39:35 +0530

    Create 10_assignment_extends.ts

---
+++ 10_assignment_extends.ts
@@ -0,0 +1,37 @@
+interface IPersonDetails {
+    id: number;
+    firstName: string;
+    lastName: string;
+    xender: string;
+    street: string
+}
+
+interface IPersonOtherDetails {
+    education: string;
+    workInCompany: string;
+    exprience: string;
+    salary: number;
+}
+
+interface IPerson extends IPersonDetails, IPersonOtherDetails {
+    age: number;
+    hobbies: string;
+    specialFood: string;
+}
+
+var personInformation: IPerson = {
+    id: 124,
+    firstName: "Dipali",
+    lastName: "Pawar",
+    xender: "Female",
+    street: "Islampur",
+    education: "M.C.A",
+    workInCompany: "Gurukul PVT LTD",
+    exprience: "2 year",
+    salary: 20000,
+    age: 26,
+    hobbies: "Reading",
+    specialFood: "Masala Pulav"
+}
+console.log("Person Details :", personInformation);
+
