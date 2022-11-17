fe0a2063be2db119818da4c60e4d78ee8006d478
Dpawar2310 <dipalisanpawar@gmail.com>
2022-11-17 21:37:58 +0530

    Create 04_stringType.js

---
+++ 04_stringType.js
@@ -0,0 +1,9 @@
+// var username : string = "CodeMind Technology";
+//var s1 = `This is ${username}`;
+//console.log(s1);
+var username = 'CodeMind Techonology';
+var s1 = "My Name is $ {username}";
+console.log(username);
+console.log(s1.indexOf('n')); //output
+console.log(s1.lastIndexOf('n')); //output
+console.log(s1.charAt((0)));
