e7bbb0f360ef5ab8603d709c017418004cdafded
Dpawar2310 <dipalisanpawar@gmail.com>
2022-11-17 21:39:29 +0530

    Create 10_assignment_extends.js

---
+++ 10_assignment_extends.js
@@ -0,0 +1,15 @@
+var personInformation = {
+    id: 124,
+    firstName: "Dipali",
+    lastName: "Pawar",
+    xender: "Female",
+    street: "Islampur",
+    education: "M.C.A",
+    workInCompany: "Gurukul PVT LTD",
+    exprience: "2 year",
+    salary: 20000,
+    age: 26,
+    hobbies: "Reading",
+    specialFood: "Masala Pulav"
+};
+console.log("Person Details :", personInformation);
