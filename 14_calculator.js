953f32071cf72d8e97cf7c4400ce81c33d80c3a2
Dpawar2310 <dipalisanpawar@gmail.com>
2022-11-19 21:53:53 +0530

    Create 14_calculator.js

---
+++ 14_calculator.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var _14_cal_1 = require("./14_cal");
+var objCal = new _14_cal_1.Calculator();
+console.log(objCal.add(2, 3));
+console.log(objCal.sub(10, 5));
+var courses = ["Angular", "React", "Nodejs"];
+console.log(courses.toString());
+var e1;
+var e2 = { id: 123, name: "John" };
+//e1 = e2;  //Type Casting
+//e2 = e1; //
